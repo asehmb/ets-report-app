d0d02ce0fc91c24b8fd7d1c4149f69f716fe93d8
asehmb <130265153+asehmb@users.noreply.github.com>
2025-03-01 15:42:13 -0700

    working on parsing data from jsonify

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ def process():
     bus_database = Databases()
     data = request.get_json() # retrieve the data sent from JavaScript
     # process the data using Python code
-    result = data['value'] * 2
+    result = 0;
     if data['value'] != "":
         print(bus_database.get_a_bus_stop(data['value']))
         bus_database.update_to_bus_database(data['value'], 1)
-        print(bus_database.get_a_bus_stop(data['value']))
+        result = bus_database.get_a_bus_stop(data['value'])
     return jsonify(result=result) # return the result to JavaScript
 
 
--- static/js/index.js
+++ static/js/index.js
@@ -6,12 +6,15 @@ function myMap() {
     var mapProp= {
       center:new google.maps.LatLng(53.54610598090649, -113.49390709480268),
       zoom:12,
+      // loop
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-    let button = document.getElementById("submit_report");
+    let button = document.getElementById("search_bar");
+    
 
     button.addEventListener("click", function() {
+      console.log("button clicked");
         // let lat = document.getElementById("latitude").value;
         // let long = document.getElementById("longitude").value;
         // let description = document.getElementById("description").value;
@@ -21,7 +24,7 @@ function myMap() {
         let marker = new google.maps.Marker({
             position: new google.maps.LatLng(lat, long),
             map: map,
-            title: "description",
+            title: "Report",
             color: "red"
         });
         google.maps.event.trigger(map, 'resize');
@@ -34,9 +37,49 @@ function myMap() {
         //     infoWindow.open(map, marker);
         // });
     });
+    return map;
 }
 
 
 
 window.myMap = myMap;
+
+function sendData() {
+  var value = document.getElementById("search-bar").value;
+  $.ajax({
+      url: '/process',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ 'value': value }),
+      success: function(response) {
+          console.log(JSON.parse(response));
+      },
+      error: function(error) {
+          console.log(error);
+      }
+  });
+  return value;
+  }
+
+globalThis.sendData = sendData;
+
+function drop_pin() {
+    let lat;
+    let long;
+    let description = document.getElementById("description").value;
+    let map =window.myMap;
+
+
+
+    let marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, long),
+        map: map,
+        title: "Report",
+        color: "red"
+    });
+    google.maps.event.trigger(map, 'resize');
+
+
+}
+
 
